3f76d4201a8969ef5944132b6e76aefe2f86c1bd
manozbiswas <mkbiitdu@gmail.com>
2016-04-05 19:23:36 +0600

    final change of first pahase

--- myapp/views.py
+++ myapp/views.py
@@ -137,6 +137,8 @@ def process_graph(university):
     # Save File
     plt.savefig('resources/images/university.png')
     plt.cla()
+    plt.clf()
+    plt.close()
 
 
     # return HttpResponse("image processed")
