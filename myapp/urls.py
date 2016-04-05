01f4fbc01bdd774b187becf410d53c0131d3ef70
manozbiswas <mkbiitdu@gmail.com>
2016-04-05 13:05:07 +0600

    styleing and image fixing

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.show_university_list, name='index'),
     url(r'^details$', views.details, name='details'),
     url(r'^processdata', views.process_form_data, name='Form'),
-    url(r'^import', views.show_university_list, name='Import'),
+    # url(r'^import', views.show_university_list, name='Import'),
     # ex: /polls/5/
 
     # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
